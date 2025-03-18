9caed36fb299e810685ef7488f617705d8634016
Gabriel <gabriel.vilela@sou.unifal-mg.edu.br>
2025-03-18 15:14:08 -0300

    histograma atualizado

--- histograma/histograma.c
+++ histograma/histograma.c
@@ -37,10 +37,41 @@
          Out->px[i] = sk[In->px[i]];
      return Out;
  }
+
+ void histograma(image img) {
+    int w = img->nc;
+    int h = img->nr;
+    int max = img->ml;
+    int *pixels = img->px;
+
+    float n[max + 1]; // histograma
+    float pr[max +1]; // probabilidade acumulada
+    int T[max + 1]; // tabela transformação
+
+    // calcula o histograma
+    for (int i = 0; i < max + 1; i++)
+        n[i] = 0;
+    for (int i = 0; i < h * w; i++)
+        n[pixels[i]]++;
+
+    // probabilidade acumulada das intensidades
+    pr[0] = n[0] / (w * h);
+    for (int i = 1; i < max + 1; i++)
+        pr[i] = pr[i - 1] + n[i] / (w * h);
+
+    // vetor de transformação das intensidades
+    for (int i = 0; i < max + 1; i++)
+        sk[i] = (int)pr[i] * max;
+
+    // varredura final
+    for (int i = 0; i < w * h; i++)
+        pixels[i] = T[pixels[i]];
+    return img;
+ }
  
  void msg(char *s)
  {
-     printf("\nHistograma");
+     printf("\nEqualização de Histograma");
      printf("\n-------------------------------");
      printf("\nUso:  %s  nome-imagem[.pgm] \n\n", s);
      printf("    nome-imagem[.pgm] é o nome do arquivo da imagem \n");
